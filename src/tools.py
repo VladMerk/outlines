a6b4a240cae597ca80705c7fa89b21a8d90080fd
VladMerk <merkvladimir@gmail.com>
2025-05-20 20:51:35 +0300

    fix: DDG RateLimit 202 Exception

--- src/tools.py
+++ src/tools.py
@@ -4,22 +4,31 @@ from langchain.tools import Tool, tool
 from langchain_community.tools import DuckDuckGoSearchResults
 from langchain_community.tools.wikipedia.tool import WikipediaQueryRun
 from langchain_community.utilities import WikipediaAPIWrapper
+from langchain_core.rate_limiters import InMemoryRateLimiter
 
 warnings.catch_warnings()
 warnings.simplefilter("ignore")
+
+rate_limiter = InMemoryRateLimiter(
+    requests_per_second=0.1, check_every_n_seconds=0.1, max_bucket_size=1
+)
 
 
 @tool
 async def search_engine(query: str):
     """Search engine to the internet"""
-    search = DuckDuckGoSearchResults(num_results=5)
+    search = DuckDuckGoSearchResults(
+        num_results=2,
+    )
+    await rate_limiter.aacquire()
+
     return await search.arun(query)
 
 
 wikipedia_tool = Tool(
     name="Wikipedia",
     func=WikipediaQueryRun(
-        api_wrapper=WikipediaAPIWrapper(top_k_results=5)  # type: ignore
+        api_wrapper=WikipediaAPIWrapper(top_k_results=2)  # type: ignore
     ).run,
     description="Поиск по Wikipedia",
     # verbose=True,
